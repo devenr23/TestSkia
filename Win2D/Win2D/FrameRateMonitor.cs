00884fccd5348df64806417983ed524888945a65
Deven Roberts <deven.roberts@asirobots.com>
2024-01-02 08:48:57 -0700

    Added Win2D solution

---
+++ Win2D/Win2D/FrameRateMonitor.cs
@@ -0,0 +1,87 @@
+﻿using System.Diagnostics;
+using Windows.UI.Xaml.Media;
+
+public class FrameRateMonitor
+{
+	private readonly Stopwatch _renderMonitorStopwatch;
+	private int _renderCount = 0;
+
+	private readonly Stopwatch _drawMonitorStopwatch;
+	private int _drawCount = 0;
+
+	public FrameRateMonitor()
+	{
+		_renderMonitorStopwatch = new Stopwatch();
+		_drawMonitorStopwatch = new Stopwatch();
+
+		Start();
+	}
+
+	public void Start()
+	{
+		_renderMonitorStopwatch.Start();
+		_renderCount = 0;
+
+		_drawMonitorStopwatch.Start();
+		_drawCount = 0;
+
+		CompositionTarget.Rendering += CompositionTargetOnRendering;
+	}
+
+	/// <summary>
+	/// This is called every time WPF is about to render a frame
+	/// </summary>
+	private void CompositionTargetOnRendering(object sender, object e)
+	{
+		lock (_renderMonitorStopwatch)
+		{
+			_renderCount++;
+
+			var elapsedSeconds = _renderMonitorStopwatch.ElapsedMilliseconds / 1000.0;
+			if (elapsedSeconds > 0.05)
+			{
+				FrameRate = _renderCount / elapsedSeconds;
+			}
+
+			if (elapsedSeconds > 1)
+			{
+				_renderCount = 0;
+				_renderMonitorStopwatch.Restart();
+			}
+		}
+	}
+	
+	public void DrawCalled()
+	{
+		lock (_drawMonitorStopwatch)
+		{
+			_drawCount++;
+
+			var elapsedSeconds = _drawMonitorStopwatch.ElapsedMilliseconds / 1000.0;
+			if (elapsedSeconds > 0.05)
+			{
+				DrawRate = _drawCount / elapsedSeconds;
+			}
+
+			if (elapsedSeconds > 1)
+			{
+				_drawCount = 0;
+				_drawMonitorStopwatch.Restart();
+			}
+		}
+	}
+
+	public double FrameRate { get; private set; }
+	public double DrawRate { get; private set; }
+	
+	public void Stop()
+	{
+		CompositionTarget.Rendering -= CompositionTargetOnRendering;
+		
+		_renderMonitorStopwatch.Stop();
+		_renderCount = 0;
+		
+		_drawMonitorStopwatch.Stop();
+		_drawCount = 0;
+	}
+}
---
+++ Win2D/Win2D/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace Win2D
+{
+	public sealed partial class MainPage : Page
+	{
+		private readonly Random _random = new Random();
+		private readonly FrameRateMonitor _frameRateMonitor = new FrameRateMonitor();
+
+		private const int NumberOfLines = 5_000;
+
+		public MainPage()
+		{
+			this.InitializeComponent();
+
+			CompositionTarget.Rendering += CompositionTarget_Rendering;
+
+			_frameRateMonitor.Start();
+			
+		}
+
+		private void CompositionTarget_Rendering(object sender, object e)
+		{
+			MyCanvas.Invalidate();
+		}
+
+		public void CanvasControl_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+		{
+			var session = args.DrawingSession;
+
+			for (int i = 0; i < 5;/* NumberOfLines;*/ i++)
+			{
+				var width = (int)sender.ActualWidth;
+				var height = (int)sender.ActualHeight;
+
+				var point1 = RandomPoint(width, height);
+				var point2 = RandomPoint(width, height);
+				var color = RandomColor();
+				var strokeWidth = _random.Next(1, 10);
+
+				session.DrawLine(point1, point2, color, strokeWidth);
+			}
+
+			_frameRateMonitor.DrawCalled();
+
+			var frameRate = _frameRateMonitor.FrameRate;
+			var drawRate = _frameRateMonitor.DrawRate;
+
+			session.DrawText($"Frame Rate: {frameRate:0.0} - Draw Rate: {drawRate:0.0}", new Vector2(0, 0), Colors.Black);
+		}
+
+		private Vector2 RandomPoint(int width, int height)
+		{
+			return new Vector2(_random.Next(width), _random.Next(height));
+		}
+
+		private Color RandomColor()
+		{
+			return Color.FromArgb(255, (byte)_random.Next(255), (byte)_random.Next(255), (byte)_random.Next(255));
+		}
+	}
+}
